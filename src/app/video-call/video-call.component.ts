99dd8a917464248e3075715c92d1de7ac13cff17
purnimasaikoumudi <pangulurikoumudi@gmail.com>
2020-09-16 15:32:23 +0530

    minor changes updated

--- src/app/video-call/video-call.component.ts
+++ src/app/video-call/video-call.component.ts
@@ -20,12 +20,12 @@ export class VideoCallComponent implements OnInit {
   };
   ngOnInit(): void {
     const config = {
-      apiKey: "AIzaSyAOhJiW1rootijHQaOBFglnySJk5Kh0fag",
-      authDomain: "fir-rtc-70816.firebaseapp.com",
-      databaseURL: "https://fir-rtc-70816.firebaseio.com",
-      projectId: "fir-rtc-70816",
-      storageBucket: "fir-rtc-70816.appspot.com",
-      messagingSenderId: "157802444350",
+      apiKey: "AIzaSyB7tTYfwZsb6admw0Q2f1zC3kGBnk9lUis",
+      authDomain: "koumirtc.firebaseapp.com",
+      databaseURL: "https://koumirtc.firebaseio.com",
+      projectId: "koumirtc",
+      storageBucket: "koumirtc.appspot.com",
+      messagingSenderId: "926790374930",
     };
     firebase.initializeApp(config);
     navigator.mediaDevices.getUserMedia({audio:true, video:true})
@@ -37,7 +37,7 @@ export class VideoCallComponent implements OnInit {
     this.id = Math.floor(Math.random()*1000000000);
     this.pc = new RTCPeerConnection(this.servers);
     console.log("hh",this.pc);
-    this.pc.onicecandidate = (event => event.candidate?this.sendMessage(1, JSON.stringify({'ice': event.candidate})):console.log("Sent All Ice") );
+    this.pc.onicecandidate = (event => event.candidate?this.sendMessage(this.id, JSON.stringify({'ice': event.candidate})):console.log("Sent All Ice") );
     this.pc.onaddstream  = (event => this.clientVideo.srcObject = event.stream)
     this.database.on('child_added', this.readMessage)
   }
@@ -49,26 +49,25 @@ export class VideoCallComponent implements OnInit {
   }
 
   sendMessage(senderId, data) {
-    this.id = senderId;
-    console.log("senderId",senderId,"data",data)
+   
     let msg = this.database.push({ sender: senderId, message: data });
-    setTimeout(()=>{
-      msg.remove();
-    },5000); 
+    // setTimeout(()=>{
+    //   msg.remove();
+    // },100); 
    }
 
    readMessage(data) {
     let msg = JSON.parse(data.val().message);
     let sender = data.val().sender;
     console.log("data from database",data,sender)
-    if (sender !== 1) {
+    if (sender !== this.id) {
     if (msg.ice != undefined)
     this.pc.addIceCandidate(new RTCIceCandidate(msg.ice));
     else if (msg.sdp.type == "offer")
     this.pc.setRemoteDescription(new RTCSessionDescription(msg.sdp))
     .then(() => this.pc.createAnswer())
     .then(answer => this.pc.setLocalDescription(answer))
-    .then(() => this.sendMessage(2, JSON.stringify({'sdp': this.pc.localDescription})));
+    .then(() => this.sendMessage(this.id, JSON.stringify({'sdp': this.pc.localDescription})));
     else if (msg.sdp.type == "answer")
     this.pc.setRemoteDescription(new RTCSessionDescription(msg.sdp));
     }
